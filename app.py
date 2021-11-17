dd2266001a02f1419fde167f58710c971b622a64
oneazi <omairneazi2000@gmail.com>
2021-11-17 13:22:13 -0700

    Added query for metric and artist combined

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ CORS(app)
 conn = psycopg2.connect(host='localhost', dbname='spotify_data', password='', user='', port=8888)
 cur = conn.cursor()
 
-@app.route("/spotifyData")
+# used for getting songs according to a metric without any artist specified
+@app.route("/metric")
 def get_tracks():
     metric = request.args.get('metric').lower()
     query = "SELECT Artist.Alias, Album.AlbumName, Label.LabelName, T.SongName, T.{}\
@@ -21,6 +22,21 @@ def get_tracks():
             WHERE T.AlbumID=Album.AlbumID AND Album.ArtistID=Artist.ArtistID AND Album.LabelID = Label.LabelID\
             ORDER BY T.{} DESC;"
     cur.execute(sql.SQL(query).format(sql.Identifier(metric), sql.Identifier(metric), sql.Identifier(metric)))
+    results = cur.fetchall()
+    return jsonify({'results': results})
+
+# used for getting songs according to a metric and artist
+@app.route("/metric/artist")
+def get_tracks_by_artist():
+    metric = request.args.get('metric').lower()
+    artist = request.args.get('artist')
+    query = "SELECT T.Alias, T.AlbumName, Label.LabelName, T.SongName, T.{}\
+            FROM Label, (SELECT * FROM Track, Album, Artist \
+                            WHERE Track.AlbumID=Album.AlbumID AND Album.ArtistID=Artist.ArtistID AND Artist.Alias=%s\
+                            ORDER BY Track.{} DESC LIMIT 10) as T\
+            WHERE T.LabelID = Label.LabelID\
+            ORDER BY T.{} DESC;"
+    cur.execute(sql.SQL(query).format(sql.Identifier(metric), sql.Identifier(metric), sql.Identifier(metric)), (artist,))
     results = cur.fetchall()
     return jsonify({'results': results})
 
