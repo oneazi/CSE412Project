d2b990f3a2ef42975905ab176d8ba690d9280455
oneazi <omairneazi2000@gmail.com>
2021-11-17 17:01:48 -0700

    Initial working barchart

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var barWidth;
 var barHeight;
 var barInnerWidth;
 var barInnerHeight;
-var barMargin = {top: 20, right: 60, bottom: 60, left: 100}
+var barMargin = {top: 40, right: 40, bottom: 80, left: 100}
 
 var toolTip;  
 
@@ -28,15 +28,133 @@ function checkOption() {
     }
 }
 
+// hepler function to capitalize the first letter in a word
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 // draws the bar chart 
 function drawBar()
 {
     const currentMetric = document.getElementById('metric-select').value;
+    const currentArtist = document.getElementById('name-input').value.trim();
 
-    fetch(`http://localhost:5000/spotifyData?metric=${currentMetric}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(() => {
-        console.log("Failed to contact to the server.")
-    });
+    if (currentMetric === 'artist') {
+        // TODO: implement for top artists
+    }
+    else if (currentMetric === 'album') {
+        // TODO: implement for top albums and top albums by specific artist
+        if (currentArtist === '')
+        {
+            console.log("no artist")
+        }
+        else {
+            console.log('artist found')
+        }
+    }
+    else {
+        if (currentArtist === '')
+        {
+            fetch(`http://localhost:5000/metric?metric=${currentMetric}`)
+            .then(response => response.json())
+            .then(data => drawMetricBar(data, currentMetric, currentArtist))
+            .catch((error) => {
+                console.log(error)
+            });
+        }
+        else {
+            fetch(`http://localhost:5000/metric/artist?metric=${currentMetric}&artist=${currentArtist}`)
+            .then(response => response.json())
+            .then(data => drawMetricBar(data, currentMetric, currentArtist))
+            .catch((error) => {
+                console.log(error)
+            });
+        }
+        
+    }
+}
+
+// TODO: handle case where array is empty aka Artist Not Found
+function drawMetricBar(data, currentMetric, currentArtist) {
+    data = data['results']
+    const xScale = d3.scaleLinear()
+                    .domain([0, d3.max(data, function(d) {  return d[4]; })])
+                    .range([0, barInnerWidth]);
+    const yScale = d3.scaleBand()
+                        .domain(data.map(function(d) { return d[3];}))
+                        .range([0, barInnerHeight])
+                        .padding(0.1);
+    
+    barSvg.select('g').remove();
+    
+    const g = barSvg.append('g')
+            .attr('transform', 'translate('+barMargin.left+', '+barMargin.top+')');
+
+    g.selectAll('g')
+        .data(data)
+        .join(
+            enter => {
+                const glyph = enter.append('g')
+                            .attr('transform', (d) => `translate(${xScale(0)}, ${yScale(d[3])})`);
+                glyph.append('rect')
+                    .attr('height', yScale.bandwidth())
+                    .attr('width', xScale(0));
+
+                glyph.selectAll("rect")
+                    .transition()
+                    .delay(function () {return Math.random()*1000 + 100;})
+                    .duration(1000)
+                    .attr("width", function(d) { return xScale(d[4]); });
+                    
+                glyph.append('text')
+                    .transition()
+                    .delay(1100)
+                    .attr('x', (d) => xScale(d[4])/2)
+                    .attr('y', yScale.bandwidth()/2)
+                    .style('fill', 'black')
+                    .style('alignment-baseline','middle')
+                    .style('text-anchor','middle')
+                    .text(d => d[3]);
+            }
+        )
+
+    // add all axis labels, titles, and scales
+    const yAxis = d3.axisLeft(yScale);
+    var gYAxis = g.append('g').call(yAxis);
+    gYAxis.selectAll('text, .tick line').remove();
+
+    const xAxis = d3.axisBottom(xScale);
+    var gXAxis = g.append('g').call(xAxis);                 
+    gXAxis.attr('transform',`translate(0,${barInnerHeight})`)
+    .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-10px")
+        .attr("dy", "0px")
+        .attr("transform", "rotate(-45)" );
+    
+    g.append('text')
+        .attr('class','axis-label')
+        .attr('text-anchor','middle')
+        .attr('x', barInnerWidth/2)
+        .attr('y', barInnerHeight+70)
+        .text(`${capitalizeFirstLetter(currentMetric)}`)    
+    
+    let header = `Top 10 songs ranked by ${capitalizeFirstLetter(currentMetric)}`;
+    if (currentArtist !== '') {
+        header = header + ` for ${currentArtist}`
+    }
+    g.append('text')
+        .attr('class','title')
+        .attr('text-anchor','middle')
+        .attr('y', -10)
+        .attr('x', barInnerWidth/2)
+        .text(header);
+    
+    g.append('text')
+        .attr('class','title')
+        .attr('text-anchor','middle')
+        .attr('y','-30px')
+        .attr('x',-barInnerHeight/2)
+        .attr("transform", "rotate(-90)" )
+        .text(`Songs`);
 }
