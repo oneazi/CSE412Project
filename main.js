b5e7eff1a54d4f585abd14bc42641e00c6109994
oneazi <omairneazi2000@gmail.com>
2021-11-22 15:35:06 -0700

    Completed Graphs with Functionality

--- main.js
+++ main.js
@@ -20,8 +20,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // disable the input textbox if "artist" is the value chosen in the dropdown
 function checkOption() {
-    if (document.getElementById('metric-select').value === 'artist') {
+    if (document.getElementById('metric-select').value === 'artistp' || document.getElementById('metric-select').value === 'artistf') {
         document.getElementById('name-input').disabled = true;
+        document.getElementById('name-input').value = '';
     }
     else {
         document.getElementById('name-input').disabled = false;
@@ -39,29 +40,28 @@ function drawBar()
     const currentMetric = document.getElementById('metric-select').value;
     const currentArtist = document.getElementById('name-input').value.trim();
 
-    if (currentMetric === 'artist') {
-        // TODO: implement for top artists
+    if (currentMetric === 'artistf') {
         fetch(`http://localhost:5000/artist/followers`)
         .then(response => response.json())
-        .then(data => drawMetricBar(data, currentMetric, currentArtist))
+        .then(data => drawArtistBar(data, 'Followers'))
         .catch((error) => {
             console.log(error)
         });
-
+    }
+    else if (currentMetric === 'artistp') {
         fetch(`http://localhost:5000/artist/popularity`)
         .then(response => response.json())
-        .then(data => drawMetricBar(data, currentMetric, currentArtist))
+        .then(data => drawArtistBar(data, 'Popularity'))
         .catch((error) => {
             console.log(error)
         });
     }
     else if (currentMetric === 'album') {
-        // TODO: implement for top albums and top albums by specific artist
         if (currentArtist === '')
         {
             fetch(`http://localhost:5000/album`)
             .then(response => response.json())
-            .then(data => drawMetricBar(data, currentMetric, currentArtist))
+            .then(data => drawAlbumBar(data, currentArtist))
             .catch((error) => {
                 console.log(error)
             });
@@ -69,7 +69,7 @@ function drawBar()
         else {
             fetch(`http://localhost:5000/album/artist?artist=${currentArtist}`)
             .then(response => response.json())
-            .then(data => drawMetricBar(data, currentMetric, currentArtist))
+            .then(data => drawAlbumBar(data, currentArtist))
             .catch((error) => {
                 console.log(error)
             });
@@ -97,9 +97,9 @@ function drawBar()
     }
 }
 
-// TODO: handle case where array is empty aka Artist Not Found
 function drawMetricBar(data, currentMetric, currentArtist) {
     data = data['results']
+    console.log(data)
     const xScale = d3.scaleLinear()
                     .domain([0, d3.max(data, function(d) {  return d[4]; })])
                     .range([0, barInnerWidth]);
@@ -118,7 +118,35 @@ function drawMetricBar(data, currentMetric, currentArtist) {
         .join(
             enter => {
                 const glyph = enter.append('g')
-                            .attr('transform', (d) => `translate(${xScale(0)}, ${yScale(d[3])})`);
+                            .attr('transform', (d) => `translate(${xScale(0)}, ${yScale(d[3])})`)
+                            .on('mouseover', function(d) {
+                                toolTip.transition()
+                                    .duration(50)
+                                    .style("opacity", 1);
+                                let toolTipText = `Artist: ${d[0]}<br>Album: ${d[1]}<br>Song: ${d[3]}<br>Label: ${d[2]}<br>${capitalizeFirstLetter(currentMetric)}: ${d[4]}`;
+                                toolTip.html(toolTipText)
+                                    .style("left", (d3.event.pageX + 10) + "px")
+                                    .style("top", (d3.event.pageY - 15) + "px");
+                                })
+                                .on('mousemove',function(d) {
+                                toolTip.transition()
+                                    .duration(50)
+                                    .style("opacity", 1);
+                                let toolTipText = `Artist: ${d[0]}<br>Album: ${d[1]}<br>Song: ${d[3]}<br>Label: ${d[2]}<br>${capitalizeFirstLetter(currentMetric)}: ${d[4]}`;
+                                toolTip.html(toolTipText)
+                                    .style("left", (d3.event.pageX + 10) + "px")
+                                    .style("top", (d3.event.pageY - 15) + "px");
+                                })
+                                .on('mouseout', () => {
+                                toolTip.transition()
+                                    .duration(50)
+                                    .style("opacity", 0);
+                                })
+                                .on('click', function(d) {
+                                    let query = `${d[0]} ${d[3]}`
+                                    let url ='http://www.google.com/search?q=' + query;
+                                    window.open(url);
+                                });
                 glyph.append('rect')
                     .attr('height', yScale.bandwidth())
                     .attr('width', xScale(0));
@@ -162,7 +190,7 @@ function drawMetricBar(data, currentMetric, currentArtist) {
         .attr('y', barInnerHeight+70)
         .text(`${capitalizeFirstLetter(currentMetric)}`)    
     
-    let header = `Top 10 songs ranked by ${capitalizeFirstLetter(currentMetric)}`;
+    let header = `Top songs ranked by ${capitalizeFirstLetter(currentMetric)}`;
     if (currentArtist !== '') {
         header = header + ` for ${currentArtist}`
     }
@@ -180,4 +208,227 @@ function drawMetricBar(data, currentMetric, currentArtist) {
         .attr('x',-barInnerHeight/2)
         .attr("transform", "rotate(-90)" )
         .text(`Songs`);
+}
+
+function drawArtistBar(data, currentMetric) {
+    data = data['results']
+    console.log(data)
+    const xScale = d3.scaleLinear()
+                    .domain([0, d3.max(data, function(d) {  return d[1]; })])
+                    .range([0, barInnerWidth]);
+    const yScale = d3.scaleBand()
+                        .domain(data.map(function(d) { return d[0];}))
+                        .range([0, barInnerHeight])
+                        .padding(0.1);
+    
+    barSvg.select('g').remove();
+    
+    const g = barSvg.append('g')
+            .attr('transform', 'translate('+barMargin.left+', '+barMargin.top+')');
+
+    g.selectAll('g')
+        .data(data)
+        .join(
+            enter => {
+                const glyph = enter.append('g')
+                            .attr('transform', (d) => `translate(${xScale(0)}, ${yScale(d[0])})`)
+                            .on('mouseover', function(d) {
+                                toolTip.transition()
+                                    .duration(50)
+                                    .style("opacity", 1);
+                                let toolTipText = `Artist: ${d[0]}<br>${currentMetric}: ${d[1]}`;
+                                toolTip.html(toolTipText)
+                                    .style("left", (d3.event.pageX + 10) + "px")
+                                    .style("top", (d3.event.pageY - 15) + "px");
+                                })
+                                .on('mousemove',function(d) {
+                                toolTip.transition()
+                                    .duration(50)
+                                    .style("opacity", 1);
+                                let toolTipText = `Artist: ${d[0]}<br>${currentMetric}: ${d[1]}`;
+                                toolTip.html(toolTipText)
+                                    .style("left", (d3.event.pageX + 10) + "px")
+                                    .style("top", (d3.event.pageY - 15) + "px");
+                                })
+                                .on('mouseout', () => {
+                                toolTip.transition()
+                                    .duration(50)
+                                    .style("opacity", 0);
+                                })
+                                .on('click', function(d) {
+                                    let query = `${d[0]}`
+                                    let url ='http://www.google.com/search?q=' + query;
+                                    window.open(url);
+                                });
+                glyph.append('rect')
+                    .attr('height', yScale.bandwidth())
+                    .attr('width', xScale(0));
+
+                glyph.selectAll("rect")
+                    .transition()
+                    .delay(function () {return Math.random()*1000 + 100;})
+                    .duration(1000)
+                    .attr("width", function(d) { return xScale(d[1]); });
+                    
+                glyph.append('text')
+                    .transition()
+                    .delay(1100)
+                    .attr('x', (d) => xScale(d[1])/2)
+                    .attr('y', yScale.bandwidth()/2)
+                    .style('fill', 'black')
+                    .style('alignment-baseline','middle')
+                    .style('text-anchor','middle')
+                    .text(d => d[0]);
+            }
+        )
+
+    // add all axis labels, titles, and scales
+    const yAxis = d3.axisLeft(yScale);
+    var gYAxis = g.append('g').call(yAxis);
+    gYAxis.selectAll('text, .tick line').remove();
+
+    const xAxis = d3.axisBottom(xScale);
+    var gXAxis = g.append('g').call(xAxis);                 
+    gXAxis.attr('transform',`translate(0,${barInnerHeight})`)
+    .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-10px")
+        .attr("dy", "0px")
+        .attr("transform", "rotate(-45)" );
+    
+    g.append('text')
+        .attr('class','axis-label')
+        .attr('text-anchor','middle')
+        .attr('x', barInnerWidth/2)
+        .attr('y', barInnerHeight+70)
+        .text(`${currentMetric}`)    
+    
+    let header = `Top artists ranked by ${currentMetric}`;
+    g.append('text')
+        .attr('class','title')
+        .attr('text-anchor','middle')
+        .attr('y', -10)
+        .attr('x', barInnerWidth/2)
+        .text(header);
+    
+    g.append('text')
+        .attr('class','title')
+        .attr('text-anchor','middle')
+        .attr('y','-30px')
+        .attr('x',-barInnerHeight/2)
+        .attr("transform", "rotate(-90)" )
+        .text(`Artists`);
+}
+
+function drawAlbumBar(data, currentArtist) {
+    data = data['results']
+    console.log(data)
+    const xScale = d3.scaleLinear()
+                    .domain([0, d3.max(data, function(d) {  return d[1]; })])
+                    .range([0, barInnerWidth]);
+    const yScale = d3.scaleBand()
+                        .domain(data.map(function(d) { return d[0];}))
+                        .range([0, barInnerHeight])
+                        .padding(0.1);
+    
+    barSvg.select('g').remove();
+    
+    const g = barSvg.append('g')
+            .attr('transform', 'translate('+barMargin.left+', '+barMargin.top+')');
+
+    g.selectAll('g')
+        .data(data)
+        .join(
+            enter => {
+                const glyph = enter.append('g')
+                            .attr('transform', (d) => `translate(${xScale(0)}, ${yScale(d[0])})`)
+                            .on('mouseover', function(d) {
+                                toolTip.transition()
+                                    .duration(50)
+                                    .style("opacity", 1);
+                                let toolTipText = `Artist: ${d[2]}<br>Album: ${d[0]}<br>Popularity: ${d[1]}`;
+                                toolTip.html(toolTipText)
+                                    .style("left", (d3.event.pageX + 10) + "px")
+                                    .style("top", (d3.event.pageY - 15) + "px");
+                                })
+                                .on('mousemove',function(d) {
+                                toolTip.transition()
+                                    .duration(50)
+                                    .style("opacity", 1);
+                                let toolTipText = `Artist: ${d[2]}<br>Album: ${d[0]}<br>Popularity: ${d[1]}`;
+                                toolTip.html(toolTipText)
+                                    .style("left", (d3.event.pageX + 10) + "px")
+                                    .style("top", (d3.event.pageY - 15) + "px");
+                                })
+                                .on('mouseout', () => {
+                                toolTip.transition()
+                                    .duration(50)
+                                    .style("opacity", 0);
+                                })
+                                .on('click', function(d) {
+                                    let query = `${d[2]} ${d[0]}`
+                                    let url ='http://www.google.com/search?q=' + query;
+                                    window.open(url);
+                                });
+                glyph.append('rect')
+                    .attr('height', yScale.bandwidth())
+                    .attr('width', xScale(0));
+
+                glyph.selectAll("rect")
+                    .transition()
+                    .delay(function () {return Math.random()*1000 + 100;})
+                    .duration(1000)
+                    .attr("width", function(d) { return xScale(d[1]); });
+                    
+                glyph.append('text')
+                    .transition()
+                    .delay(1100)
+                    .attr('x', (d) => xScale(d[1])/2)
+                    .attr('y', yScale.bandwidth()/2)
+                    .style('fill', 'black')
+                    .style('alignment-baseline','middle')
+                    .style('text-anchor','middle')
+                    .text(d => d[0]);
+            }
+        )
+
+    // add all axis labels, titles, and scales
+    const yAxis = d3.axisLeft(yScale);
+    var gYAxis = g.append('g').call(yAxis);
+    gYAxis.selectAll('text, .tick line').remove();
+
+    const xAxis = d3.axisBottom(xScale);
+    var gXAxis = g.append('g').call(xAxis);                 
+    gXAxis.attr('transform',`translate(0,${barInnerHeight})`)
+    .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-10px")
+        .attr("dy", "0px")
+        .attr("transform", "rotate(-45)" );
+    
+    g.append('text')
+        .attr('class','axis-label')
+        .attr('text-anchor','middle')
+        .attr('x', barInnerWidth/2)
+        .attr('y', barInnerHeight+70)
+        .text(`Popularity`)
+    
+    let header = `Top albums ranked by Popularity`;
+    if (currentArtist !== '') {
+        header = header + ` for ${currentArtist}`
+    }
+    g.append('text')
+        .attr('class','title')
+        .attr('text-anchor','middle')
+        .attr('y', -10)
+        .attr('x', barInnerWidth/2)
+        .text(header);
+    
+    g.append('text')
+        .attr('class','title')
+        .attr('text-anchor','middle')
+        .attr('y','-30px')
+        .attr('x',-barInnerHeight/2)
+        .attr("transform", "rotate(-90)" )
+        .text(`Albums`);
 }
